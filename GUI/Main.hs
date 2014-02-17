49917c90fed32bfa2abe01419a631347704ee083
alexgadea <alex.aegf@gmail.com>
2014-02-17 19:37:12 -0300

    Agregando robustes en relación a los archivos pasados por parametros al iniciar fun-gui.

--- GUI/Main.hs
+++ GUI/Main.hs
@@ -3,7 +3,11 @@ module Main where
 import System.Environment (getArgs)
 
 import Graphics.UI.Gtk hiding (get)
+
 import System.Glib
+import System.Directory
+
+import Control.Monad
 
 import GUI.Gui
 import Paths_fun_gui
@@ -21,6 +25,30 @@ main = do
     
     args <- getArgs
     
-    _ <- mainFunGui xml args
+    (existingFiles,errFiles) <- checkArgs args
+    
+    _ <- when (not $ null errFiles) (errFilesMsg errFiles)
+    
+    _ <- mainFunGui xml existingFiles
     
     mainGUI
+
+checkArgs :: [String] -> IO ([String],[String])
+checkArgs = foldM check ([],[])
+    where
+        check :: ([String],[String]) -> String -> IO ([String],[String])
+        check (fp,er) arg = doesFileExist arg >>= \exist ->
+                            if exist
+                                then return (arg:fp,er)
+                                else return (fp,arg:er)
+
+errFilesMsg :: [String] -> IO ()
+errFilesMsg errFiles = putStrLn $ 
+                        unwords [ "Los siguientes archivos no existen o"
+                                , "fallaron al intentar abrir:"
+                                , foldr concatf "" errFiles
+                                ]
+    where
+        concatf :: String -> String -> String
+        concatf errf "" = errf
+        concatf errf err = errf++", "++err
